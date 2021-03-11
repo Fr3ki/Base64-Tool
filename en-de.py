ce5bcca9474328219f7cab843e0af8c81faabd7c
Fr3ki <79277183+Fr3ki@users.noreply.github.com>
2021-03-10 20:30:34 -0700

    Add files via upload

---
+++ en-de.py
@@ -0,0 +1,62 @@
+#Import
+from base64 import *
+
+#A surprise tool that will help us later
+FiveX = 1
+
+#Picking a tool
+EnDe = input('Encode or Decode? ')
+
+#Encode option
+if EnDe == "Encode":
+   
+    #User input and converting to "byte like object"
+    EnInput = input('To be encoded: ')
+    EnInput = bytes(EnInput, 'utf-8')
+    
+    #How many times to repeat
+    EnRepeat = input('Encode how many times? ')
+    EnRepeat = int(EnRepeat)
+
+    while FiveX <= EnRepeat:
+        
+        #Encode the user input
+        Encoded = standard_b64encode(EnInput)
+        
+        FiveX += 1
+        
+        EnInput = Encoded
+   
+    #Print result
+    print("Please excuse the b''")
+    print(Encoded)
+
+
+#Decode option
+elif EnDe == "Decode":
+
+    #User input and converting to "byte like object"
+    DeInput = input("To be decoded: ")
+    DeInput = bytes(DeInput, 'utf-8')
+
+    #How many times to repeat
+    DeRepeat = input("Decode how many times? ")
+    DeRepeat = int(DeRepeat)
+    
+    #Decode the user input
+    while FiveX <= DeRepeat:
+        
+        Decoded = standard_b64decode(DeInput)
+        
+        FiveX += 1
+
+        DeInput = Decoded
+    #Print result
+    print("Please excuse the b''")
+    print(Decoded)
+
+
+#Invalid option
+else:
+
+    print("Enter A Valid Option")
