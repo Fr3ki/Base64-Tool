da0d64b03c96c9d6a2a15669c1bab53c6485715d
Fr3ki <79277183+Fr3ki@users.noreply.github.com>
2021-04-23 04:37:48 +0000

    Major update!
    Added:
    > Help command
    > Quit command
    > File encoding and decoding
    Changed:
    > Various small code improvements for quality
    Note:
    File functionality is rough around the edges, it should work just fine, but the functions are a little junky as I wrote these in around 10 minutes during downtime in math class.

--- en-de.py
+++ en-de.py
@@ -2,43 +2,41 @@
 from base64 import *
 import os
 
-#Clear the terminal window
-os.system('cls' if os.name == 'nt' else 'clear')
-
-#A surprise tool that will help us later
-FiveX = 1
+#Clear the terminal
+os.system('clear')
 
 #Picking a tool
-EnDe = input('Encode or Decode? ')
+EnDe = input('[Encode, Decode, Help, Quit]: ')
 
-#Encode option
-if EnDe.lower() == "encode":
-   
+###########################################################
+
+#Encode function
+def Encode():
+    Repeat = 1
     #User input and converting to "byte like object"
     EnInput = input('To be encoded: ')
-    EnInput = bytes(EnInput, 'utf-8')
-    
+    EnInput = EnInput.encode()
     #How many times to repeat
     EnRepeat = input('Encode how many times? ')
     EnRepeat = int(EnRepeat)
 
-    while FiveX <= EnRepeat:
-        
+    while Repeat <= EnRepeat:
+
         #Encode the user input
         Encoded = standard_b64encode(EnInput)
-        
-        FiveX += 1
-        
+
+        Repeat += 1
+
         EnInput = Encoded
-   
+
     #Revert input to a string and print result
-    Encoded = Encoded.decode()
-    print(Encoded)
+    Output = Encoded.decode()
+    os.system('clear')
+    print(Output)
 
-
-#Decode option
-elif EnDe.lower() == "decode":
-
+#Decode function
+def Decode():
+    Repeat = 1
     #User input and converting to "byte like object"
     DeInput = input("To be decoded: ")
     DeInput = bytes(DeInput, 'utf-8')
@@ -46,24 +44,63 @@ elif EnDe.lower() == "decode":
     #How many times to repeat
     DeRepeat = input("Decode how many times? ")
     DeRepeat = int(DeRepeat)
-    
+
     #Decode the user input
-    while FiveX <= DeRepeat:
-        
+    while Repeat <= DeRepeat:
+
         Decoded = standard_b64decode(DeInput)
-        
-        FiveX += 1
+
+        Repeat += 1
 
         DeInput = Decoded
     #Revert input into a string and print result
     Decoded = Decoded.decode()
+    os.system('clear')
     print(Decoded)
 
-#Quit option
-#elif EnDe == "Quit":
+def FileEncode():
+    File = input('Name of / Path to file: ')
+    with open(File, 'r') as Opened:
+        Contents = Opened.read()
+    Writing = Contents.encode()
+    Encoded = standard_b64encode(Writing)
+    Output = Encoded.decode()
+    with open(File, 'w+') as Outfile:
+        Outfile.write(Output)
 
+def FileDecode():
+    File = input('Name of / Path to file: ')
+    with open(File, 'r') as Opened:
+        Contents = Opened.read()
+    Writing = Contents.encode()
+    Encoded = standard_b64decode(Writing)
+    Output = Encoded.decode()
+    with open (File, 'w+') as Outfile:
+        Outfile.write(Output)
+
+ ######################################################
+
+#Running encode
+if EnDe.lower() == "encode":
+     Encode()
+
+if EnDe.lower() == "encode -f":
+    FileEncode()
+
+#Running decode
+elif EnDe.lower() == "decode":
+    Decode()
+
+elif EnDe.lower() == "decode -f":
+    FileDecode()
+
+#Quit option
+elif EnDe.lower() == "quit":
+    pass
+
+elif EnDe.lower() == "help":
+    print("Base64 Multitool by Fr3ki \n Encode: Convert text to base64 (-f File mode) \n Decode: Decode Base64 into plain text (-f File mode) \n --For more information check ReadMe.md-- ")
 
 #Invalid option
 else:
-
     print("Enter A Valid Option")
